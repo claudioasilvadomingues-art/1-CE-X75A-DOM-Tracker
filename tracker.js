53a3d6aad030fa56a0d8fef41dee9d7a3063b3b2
claudioasilvadomingues-art <claudioasilvadomingues@gmail.com>
2026-03-11 21:30:40 +0100

    etape4

--- tracker.js
+++ tracker.js
@@ -1,29 +1,38 @@
 let btn_sun = document.getElementById('btn-sun');
 let sun_val = document.getElementById('sun-value');
 
-let btn_water = document.getElementById('btn-water')
-let water_value = document.getElementById('water-value')
+let btn_water = document.getElementById('btn-water');
+let water_value = document.getElementById('water-value');
 
-let break_btn = document.getElementById('btn-break')
-let break_val = document.getElementById('break-value')
+let break_btn = document.getElementById('btn-break');
+let break_val = document.getElementById('break-value');
 
-let tt_val = document.getElementById('total-value')
+let tt_val = document.getElementById('total-value');
 
-let btn_reset = document.getElementById('btn-reset')
+let btn_reset = document.getElementById('btn-reset');
+
+const max = 10;
 
 btn_sun.addEventListener('click', () => {
-    sun_val.innerText = parseInt(sun_val.innerText) + 1;
-     tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    if (parseInt(tt_val.innerText) < max){
+        sun_val.innerText = parseInt(sun_val.innerText) + 1;
+        tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    }
+
 });
 
 btn_water.addEventListener('click', () => {
-    water_value.innerText = parseInt(water_value.innerText) + 1;
-     tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    if (parseInt(tt_val.innerText) >= max){
+       water_value.innerText = parseInt(water_value.innerText) + 1;
+       tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    }
 });
 
 break_btn.addEventListener('click', () => {
-    break_val.innerText = parseInt(break_val.innerText) + 1;
-    tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    if (parseInt(tt_val.innerText) >= max){
+        break_val.innerText = parseInt(break_val.innerText) + 1;
+        tt_val.innerText = parseInt(tt_val.innerText) + 1;
+    }
 });
 
 
@@ -33,3 +42,4 @@ btn_reset.addEventListener('click', () => {
     water_value.innerText = 0;
     tt_val.innerText = 0
 });
+
