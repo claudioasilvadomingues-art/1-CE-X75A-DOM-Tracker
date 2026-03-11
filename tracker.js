1ac9e1eb61277d6f9170ed27cfe04bab28926e32
claudioasilvadomingues-art <claudioasilvadomingues@gmail.com>
2026-03-11 19:39:01 +0100

    etape2

--- tracker.js
+++ tracker.js
@@ -1,9 +1,13 @@
 let btn = document.getElementById('btn_count');
+let btn2 = document.getElementById('btn_ctn+5')
 let cnt = document.getElementById('val_count');
-
 
 btn.addEventListener('click', () => {
     cnt.innerText = parseInt(cnt.innerText) + 1;
+});
+
+btn2.addEventListener('click', () => {
+    cnt.innerText = parseInt(cnt.innerText) + 5;
 });
 
 console.log(btn);
